5c5d1b8c32de046973d497d3d76dbe5ad3fef581
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-11-13 21:47:37 -0500

    array

--- Develop/script.js
+++ Develop/script.js
@@ -20,11 +20,17 @@ function prompt() {
     length();
 }
 }
+function length() {
+
+}
 
 
 
-
-
+let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let specialCharacters = [" ", "!",`"`, "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<","=", ">", "?", "@","[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+let Array = specialCharacters.concat(uppercase, lowercase, numbers);
 
 
 
