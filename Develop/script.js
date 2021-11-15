33e524e04b36709842c35daa8ad9dde6351d898e
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-11-15 02:09:20 -0500

    save changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,91 +1,73 @@
-// Assignment Code 
+
 var generateBtn = document.querySelector("#generate");
-var uppercase;
-var lowercase;
-var numbers;
-var specialCharacters;
-var passwordLength;
+// var uppercase;
+// var lowercase;
+// var numbers;
+// var specialCharacters;
+// var passlength;
 
-// generate prompt qs
-generateBtn.addEventListener("click", choose);
-function choose() {
-  lowercase = confirm("Do you want to include lowercase letters?");
-  uppercase = confirm("Do you want to include uppercase letters?");
-  numbers = confirm("Do you want to include numbers?");
-  specialCharacters = confirm("Do you want to include special characters?");
-  if (!lowercase && !uppercase && !numbers && !specialCharacters) {
+  var lowercaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbersArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var specialCharactersArr = [" ", "!",`"`, "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<","=", ">", "?", "@","[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+// var array = specialCharacters.concat(uppercase, lowercase, numbers);
+  // generate prompt qs
+
+function generatePassword() {
+   var lengthch = prompt("The password must be between 8 and 128 characters in length");
+   while (lengthch > 128 || lengthch < 8) {
+    alert("Please enter a number between 8 and 128")
+   var lengthch = prompt("The password must be between 8 and 128 characters in length");
+  }
+   var lowercasech = confirm("Do you want to include lowercase letters?");
+   var  uppercasech = confirm("Do you want to include uppercase letters?");
+   var  numbersch = confirm("Do you want to include numbers?");
+   var specialCharactersch = confirm("Do you want to include special characters?");
+   while (!lowercasech && !uppercasech && !numbersch && !specialCharactersch) {
     alert("Please select a feature.");
-    criteria();
-} else {
-    passlength();
+    var lengthch = prompt("The password must be between 8 and 128 characters in length");
+    var lowercasech = confirm("Do you want to include lowercase letters?");
+    var  uppercasech = confirm("Do you want to include uppercase letters?");
+    var  numbersch = confirm("Do you want to include numbers?");
+    var specialCharactersch = confirm("Do you want to include special characters?");
+   };
+  
+
+
+
+     var passArr = []
+  if (lowercasech) {
+     var passArr = passArr.concat(lowercaseArr);
+  }
+  if (uppercasech) {
+     var passArr = passArr.concat(uppercaseArr);
+  }
+  if (numbersch) {
+     var passArr = passArr.concat(numbersArr);
+  }
+  if (specialCharactersch) {
+     var passArr = passArr.concat(specialCharactersArr);
+  }
+
+var password = ""
+for (var i = 0; i < lengthch; i++) {
+  password = password += passArr[Math.floor(Math.random() * passArr.length)];
 }
-}
-function passlength() {
-passlength = prompt("The password must be between 8 and 128 characters in length");
-if (passlength > 128 || passlength < 8) {
-  alert("Please enter a number between 8 and 128")
-  passlength();
-} else {
-  generatePassword();
-}
+  return password
 
 }
 
-
-
-let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-let specialCharacters = [" ", "!",`"`, "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<","=", ">", "?", "@","[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-let Array = specialCharacters.concat(uppercase, lowercase, numbers);
+// console.log(passArr);
 
 
 
-
-
-
-
+generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-}
-
-// var tagName = prompt("Please enter an HTML Tag (ex. h1, h2, p, div):", "enter tag");
-
-// if (tagName !== "h1" && tagName !== "h2" && tagName !== "p" && tagName !== "div") {
-//   alert("please enter a valid tag");
-// } else {
-//   // Creates element based on tag entered by user
-//   var tag = document.createElement(tagName);
-
-//   // Adds text content to created tag
-//   tag.textContent = "This was made via prompts. It's a " + tagName + ".";
-  
-//   // Appends tag as child of document body
-//   document.body.appendChild(tag);
-// }
-
-// var nextTag = confirm("Would you like to add another tag?");
-
-// if (nextTag === true) {
-//   var secondTagName = prompt("Please enter another  HTML Tag (ex. h1, h2, p, div):", "enter tag here");
-//   if(secondTagName !== "h1" && secondTagName !== "h2" && secondTagName !== "p" && secondTagName !== "div") {
-//     alert("please enter a valid tag");
-//   } else {
-//     var secondTag = document.createElement(secondTagName);
-//     secondTag.textContent = "This is our second tag via prompts, it's a " + secondTagName + ".";
-//     document.body.appendChild(secondTag);
-
-
-
-
-
-
-
-
-
+};
 
 
 
--- README.md
+++ README.md
@@ -8,4 +8,6 @@ AS a user
 I WANT to randomly generate a password that meets certain criteria
 SO THAT I can create a strong password that provides greater security
 ```
+## Installation
+Access this link https://nazikbestcoder.github.io/password-generator/
 
